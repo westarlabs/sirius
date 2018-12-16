1f5c4a47b8f9ea28624dccfa9d830365ed8699b4
jolestar <jolestar@gmail.com>
2018-12-16 19:53:34 +0800

    RLP serialization implement.

---
+++ sirius-core/src/main/kotlin/org/starcoin/sirius/serialization/rlp/RLP.kt
@@ -0,0 +1,44 @@
+package org.starcoin.sirius.serialization.rlp
+
+import kotlinx.serialization.*
+import kotlinx.serialization.context.SerialContext
+import kotlinx.serialization.context.SerialModule
+
+class RLP : AbstractSerialFormat(), BinaryFormat {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is RLP) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
+    override fun <T> dump(serializer: SerializationStrategy<T>, obj: T): ByteArray {
+        val rlpList = RLPList(mutableListOf())
+        val dumper = RLPOutput(rlpList)
+        dumper.encode(serializer, obj)
+        return rlpList.encode()
+    }
+
+    override fun <T> load(deserializer: DeserializationStrategy<T>, bytes: ByteArray): T {
+        val rlpList = bytes.toRLP() as RLPList
+        val loader = RLPInput(rlpList.iterator())
+        return loader.decode(deserializer)
+    }
+
+    companion object : BinaryFormat {
+
+        val plain = RLP()
+
+        override fun <T> dump(serializer: SerializationStrategy<T>, obj: T): ByteArray = plain.dump(serializer, obj)
+        override fun <T> load(deserializer: DeserializationStrategy<T>, bytes: ByteArray): T =
+            plain.load(deserializer, bytes)
+
+        override fun install(module: SerialModule) = plain.install(module)
+        override val context: SerialContext get() = plain.context
+
+    }
+}
