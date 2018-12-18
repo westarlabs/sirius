b90a4a2146cbf980f965a2047f4d6d554c5e3642
fanngyuan <fanngyuan@gmail.com>
2018-12-18 17:26:11 +0800

    add func for in memory chain

--- sirius-chainconnector/src/main/kotlin/org/starcoin/sirius/protocol/ethereum/InMemoryChain.kt
+++ sirius-chainconnector/src/main/kotlin/org/starcoin/sirius/protocol/ethereum/InMemoryChain.kt
@@ -39,7 +39,7 @@ class InMemoryChain(autoGenblock :Boolean):Chain {
     }
 
     override fun newTransaction(keyPaire:KeyPair,transaction: ChainTransaction) {
-        
+
     }
 
 }
---
+++ sirius-chainconnector/src/main/kotlin/org/starcoin/sirius/protocol/ethereum/core/RpcClient.kt
@@ -0,0 +1,10 @@
+package org.starcoin.sirius.protocol.ethereum.core
+
+class RpcClient(baseUrl : String) {
+
+    val baseUrl = baseUrl;
+
+    fun ethTransaction(){
+
+    }
+}
