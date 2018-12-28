b960b7536367ed4b4366ae996ccc539d25bc463e
tiangong3624749 <tiangong3624749@163.com>
2018-12-28 15:40:41 +0800

    Add testDeposit Method.

--- sirius-contract/src/test/kotlin/org/starcoin/sirius/contract/test/SiriusContractTest.kt
+++ sirius-contract/src/test/kotlin/org/starcoin/sirius/contract/test/SiriusContractTest.kt
@@ -6,8 +6,19 @@ import org.junit.Test
 class SiriusContractTest : ContractTestBase("sirius.sol", "SiriusService") {
 
     @Test
-    fun testCurrentEon() {
+    fun testGetCurrentEon() {
         val callResult = contract.callConstFunction("getCurrentEon")
         Assert.assertEquals(0, callResult[0] as Int)
+    }
+
+    @Test
+    fun testDeposit() {
+        val amount: Long = 10
+        val setResult = contract.callFunction(amount, "deposit", amount)
+
+        Assert.assertTrue(setResult.isSuccessful)
+        setResult.receipt.logInfoList.forEach { logInfo ->
+            println("event:$logInfo")
+        }
     }
 }
