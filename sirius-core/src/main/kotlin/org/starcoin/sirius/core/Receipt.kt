2e03196cc5ea2a60487fdb3707e10ee4c437c216
fikgol <hit.cs.lijun@gmail.com>
2019-01-02 18:20:22 +0800

    Refactor the constructor of recepit

--- sirius-core/src/main/kotlin/org/starcoin/sirius/core/Receipt.kt
+++ sirius-core/src/main/kotlin/org/starcoin/sirius/core/Receipt.kt
@@ -1,15 +1,26 @@
 package org.starcoin.sirius.core
 
+import org.starcoin.sirius.crypto.CryptoService
 import java.math.BigInteger
 
 data class Receipt(
     // NOTE: Not implement logs
-    val transactionHash: String, val transactionIndex: BigInteger,
-    val blockHash: String, val blockNumber: BigInteger,
-    val contractAddress: String, val from: String, val to: String,
+    val transactionHash: Hash, val transactionIndex: BigInteger,
+    val blockHash: Hash, val blockNumber: BigInteger,
+    val contractAddress: Address, val from: Address, val to: Address,
     val gasUsed: BigInteger, val logBloom: String, val cumulativeGasUsed: BigInteger,
     val root: String, val status: Boolean
 ) {
-
+    constructor(
+        transactionHash: String, transactionIndex: BigInteger,
+        blockHash: String, blockNumber: BigInteger,
+        contractAddress: String, from: String, to: String,
+        gasUsed: BigInteger, logBloom: String, cumulativeGasUsed: BigInteger,
+        root: String, status: Boolean
+    ) : this(
+        CryptoService.hash(transactionHash.toByteArray()), transactionIndex,
+        CryptoService.hash(blockHash.toByteArray()), blockNumber,
+        Address.wrap(contractAddress), Address.wrap(from), Address.wrap(to),
+        gasUsed, logBloom, cumulativeGasUsed, root, status
+    )
 }
-
