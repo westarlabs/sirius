e8d3f63d19b44419e70cd954f7f926f498a47bb9
jolestar <jolestar@gmail.com>
2019-01-02 11:26:06 +0800

    Refactor SiriusModelTest.

--- sirius-contract/src/test/kotlin/org/starcoin/sirius/contract/test/SiriusModelTest.kt
+++ sirius-contract/src/test/kotlin/org/starcoin/sirius/contract/test/SiriusModelTest.kt
@@ -3,9 +3,21 @@ package org.starcoin.sirius.contract.test
 import org.junit.Assert
 import org.junit.Test
 import org.starcoin.sirius.core.*
-import org.starcoin.sirius.serialization.rlp.RLP
+import org.starcoin.sirius.lang.toHEXString
+import kotlin.reflect.KClass
+import kotlin.reflect.full.companionObjectInstance
 
 class SiriusModelTest : ContractTestBase("model_test.sol", "test_all") {
+
+    fun <T : SiriusObject> doTest(siriusClass: KClass<T>, functionName: String) {
+        val companion = siriusClass.companionObjectInstance as SiriusObjectCompanion<*, *>
+        val obj = companion.mock()
+        val data = obj.toRLP()
+        val callResult = contract.callConstFunction(functionName, data)[0] as ByteArray
+        val obj1 = companion.parseFromRLP(callResult)
+        Assert.assertArrayEquals("expect ${data.toHEXString()} but get ${callResult.toHEXString()}", data, callResult)
+        Assert.assertEquals(obj, obj1)
+    }
 
     @Test
     fun testHubRootDecode() {
@@ -22,117 +34,51 @@ class SiriusModelTest : ContractTestBase("model_test.sol", "test_all") {
 
     @Test
     fun testHubRoot() {
-        val hub = HubRoot.mock()
-        val data = RLP.dump(HubRoot.serializer(), hub)
-
-        val callResult = contract.callConstFunction("hub_root_test", data)[0] as ByteArray
-        val tmp = RLP.load(HubRoot.serializer(), callResult)
-
-        assert(hub.equals(tmp))
+        doTest(HubRoot::class, "hub_root_test")
     }
 
     @Test
     fun testInitiateWithdrawal() {
-        val w = Withdrawal.mock()
-
-        val data = RLP.dump(Withdrawal.serializer(), w)
-
-        val callResult = contract.callConstFunction("initiate_withdrawal_test", data)[0] as ByteArray
-        val tmp = RLP.load(Withdrawal.serializer(), callResult)
-
-        assert(w.equals(tmp))
+        doTest(Withdrawal::class, "initiate_withdrawal_test")
     }
 
     @Test
     fun testCancelWithdrawal() {
-        val w = CancelWithdrawal.mock()
-
-        val data = RLP.dump(CancelWithdrawal.serializer(), w)
-
-        val callResult = contract.callConstFunction("cancel_withdrawal_test", data)[0] as ByteArray
-        val tmp = RLP.load(CancelWithdrawal.serializer(), callResult)
-
-        assert(w.equals(tmp))
+        doTest(CancelWithdrawal::class, "cancel_withdrawal_test")
     }
 
     @Test
     fun testBalanceUpdateChallenge() {
-        val b = BalanceUpdateChallenge.mock()
-
-        val data = RLP.dump(BalanceUpdateChallenge.serializer(), b)
-
-        val callResult = contract.callConstFunction("balance_update_challenge_test", data)[0] as ByteArray
-        val tmp = RLP.load(BalanceUpdateChallenge.serializer(), callResult)
-
-        assert(b.equals(tmp))
+        doTest(BalanceUpdateChallenge::class, "balance_update_challenge_test")
     }
 
     @Test
     fun testCloseBalanceUpdateChallenge() {
-        val close = CloseBalanceUpdateChallenge.mock()
-        val data = RLP.dump(CloseBalanceUpdateChallenge.serializer(), close)
-
-        val callResult = contract.callConstFunction("close_balance_update_challenge_test", data)[0] as ByteArray
-        val tmp = RLP.load(CloseBalanceUpdateChallenge.serializer(), callResult)
-
-        assert(close.equals(tmp))
+        doTest(CloseBalanceUpdateChallenge::class, "close_balance_update_challenge_test")
     }
 
     @Test
     fun testOpenTransferDeliveryChallengeRequest() {
-        val open = OpenTransferDeliveryChallengeRequest.mock()
-        val data = RLP.dump(OpenTransferDeliveryChallengeRequest.serializer(), open)
-
-        val callResult =
-            contract.callConstFunction("open_transfer_delivery_challenge_request_test", data)[0] as ByteArray
-        val tmp = RLP.load(OpenTransferDeliveryChallengeRequest.serializer(), callResult)
-
-        assert(open.equals(tmp))
+        doTest(OpenTransferDeliveryChallengeRequest::class, "open_transfer_delivery_challenge_request_test")
     }
 
     @Test
     fun testCloseTransferDeliveryChallenge() {
-        val close = CloseTransferDeliveryChallenge.mock()
-        val data = RLP.dump(CloseTransferDeliveryChallenge.serializer(), close)
-
-        val callResult = contract.callConstFunction("close_transfer_delivery_challenge_test", data)[0] as ByteArray
-        val tmp = RLP.load(CloseTransferDeliveryChallenge.serializer(), callResult)
-
-        assert(close.equals(tmp))
+        doTest(CloseTransferDeliveryChallenge::class, "close_transfer_delivery_challenge_test")
     }
 
     @Test
     fun testAMTreeProof() {
-        val proof = AMTreeProof.mock()
-        val data = RLP.dump(AMTreeProof.serializer(), proof)
-        val callResult = contract.callConstFunction("am_tree_proof_test", data)[0] as ByteArray
-        val tmp = RLP.load(AMTreeProof.serializer(), callResult)
-
-        assert(proof.equals(tmp))
+        doTest(AMTreeProof::class, "am_tree_proof_test")
     }
 
     @Test
     fun testAMTreePathLeafNode() {
-        val leaf = AMTreePathLeafNode.mock()
-        LOG.info(leaf.toJSON())
-        val data = RLP.dump(AMTreePathLeafNode.serializer(), leaf)
-        val callResult = contract.callConstFunction("am_tree_path_leaf_node_test", data)[0] as ByteArray
-        //LOG.info(Utils.HEX.encode(data))
-        //LOG.info(Utils.HEX.encode(callResult))
-        Assert.assertArrayEquals(data, callResult)
-        val tmp = RLP.load(AMTreePathLeafNode.serializer(), callResult)
-
-        assert(leaf.equals(tmp))
+        doTest(AMTreePathLeafNode::class, "am_tree_path_leaf_node_test")
     }
 
     @Test
     fun testBalanceUpdateProof() {
-        val proof = BalanceUpdateProof.mock()
-
-        val data = RLP.dump(BalanceUpdateProof.serializer(), proof)
-        val callResult = contract.callConstFunction("balance_update_proof_test", data)[0] as ByteArray
-        val tmp = RLP.load(BalanceUpdateProof.serializer(), callResult)
-
-        assert(proof.equals(tmp))
+        doTest(BalanceUpdateProof::class, "balance_update_proof_test")
     }
 }
--- sirius-core/src/main/kotlin/org/starcoin/sirius/lang/Extensions.kt
+++ sirius-core/src/main/kotlin/org/starcoin/sirius/lang/Extensions.kt
@@ -1,5 +1,8 @@
 package org.starcoin.sirius.lang
 
+import org.starcoin.sirius.util.Utils
 import java.math.BigInteger
 
 fun ByteArray.toULong() = BigInteger(1, this).toLong()
+
+fun ByteArray.toHEXString() = Utils.HEX.encode(this)
