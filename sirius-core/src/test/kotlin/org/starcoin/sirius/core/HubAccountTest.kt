f43bc381c44f29e7476023699791cadd409a0e83
jolestar <jolestar@gmail.com>
2019-01-15 16:14:50 +0800

    add toProto test.

--- sirius-core/src/test/kotlin/org/starcoin/sirius/core/HubAccountTest.kt
+++ sirius-core/src/test/kotlin/org/starcoin/sirius/core/HubAccountTest.kt
@@ -2,6 +2,7 @@ package org.starcoin.sirius.core
 
 import org.junit.Assert
 import org.junit.Test
+import org.starcoin.proto.Starcoin
 
 class HubAccountTest : SiriusObjectTestBase<HubAccount>(HubAccount::class) {
 
@@ -9,6 +10,8 @@ class HubAccountTest : SiriusObjectTestBase<HubAccount>(HubAccount::class) {
     fun testHubAccountProtobuf() {
         val hubAccount = HubAccount.mock()
         val bytes = hubAccount.toProtobuf()
+        val bytes1 = hubAccount.toProto<Starcoin.HubAccount>().toByteArray()
+        Assert.assertArrayEquals(bytes, bytes1)
         val hubAccount1 = HubAccount.parseFromProtobuf(bytes)
         Assert.assertEquals(hubAccount, hubAccount1)
     }
