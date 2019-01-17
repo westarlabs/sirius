f21e52423c4c25841e9c3f231d7454bafb7ec204
fikgol <hit.cs.lijun@gmail.com>
2019-01-17 15:34:16 +0800

    Add docker.sh
    Former-commit-id: 5c63067987350d6c89373e5aca9bdb3176fbd456

---
+++ sirius-chainconnector/scripts/docker.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+usage(){
+    echo "Usage $(basename $0) [clean, build, run, start, stop, logs]"
+}
+
+
+if [ $# -lt 1 ]; then
+    usage;
+fi
+
+case $"$1" in
+    clean)
+	docker rm $(docker ps -a |grep go-ethereum|awk '{print $1}')
+        ;;
+    build)
+	docker build  -f $(dirname $0)/docker/go-ethereum/Dockerfile -t starcoin/go-etherum $(dirname $0)/docker/go-ethereum/
+	;;
+    run)
+	docker run -d --name go-ethereum -p 127.0.0.1:8545:8545 -p 30303:30303 starcoin/go-etherum 
+	;;
+    logs)
+	docker logs $(docker ps -a |grep go-ethereum|awk '{print $1}') -f
+	;;
+    stop)
+	docker stop $(docker ps -a |grep go-ethereum|awk '{print $1}')
+	;;
+    start)
+	docker start $(docker ps -a |grep go-ethereum|awk '{print $1}')
+	;;
+    attach)
+	docker exec -it $(docker ps -a |grep go-ethereum|awk '{print $1}') /bin/sh
+	;;
+esac
--- sirius-chainconnector/scripts/docker/go-ethereum/start.sh
+++ sirius-chainconnector/scripts/docker/go-ethereum/start.sh
@@ -5,4 +5,4 @@ if [[ -z $init_account ]];then
     echo "Run init.sh firstly"
     exit
 fi
-geth --rpcapi personal,web3,eth,miner,txpool --mine --minerthreads=1  --cache=256 --datadir ./geth_data --etherbase $init_account 
+geth --rpcapi personal,web3,eth,miner,txpool --rpc  --rpcaddr 0.0.0.0 --mine --minerthreads=1  --cache=256 --datadir ./geth_data --etherbase $init_account 
